025d1272bc65548fd2af1a1d4923d72ce734472d
Senibo <senibodonpedro@gmail.com>
2025-05-04 22:49:17 +0100

    Learnt and implemented mehod-level security & URL-based security

---
+++ src/main/java/com/security/notes/services/UserService.java
@@ -0,0 +1,13 @@
+package com.security.notes.services;
+
+import com.security.notes.dtos.UserDTO;
+import com.security.notes.models.User;
+import java.util.List;
+
+public interface UserService {
+    void updateUserRole(Long userId, String roleName);
+
+    List<User> getAllUsers();
+
+    UserDTO getUserById(Long id);
+}
---
+++ src/main/java/com/security/notes/services/impl/UserServiceImpl.java
@@ -0,0 +1,74 @@
+package com.security.notes.services.impl;
+
+import com.security.notes.dtos.UserDTO;
+import com.security.notes.models.AppRole;
+import com.security.notes.models.Role;
+import com.security.notes.models.User;
+import com.security.notes.repositories.RoleRepository;
+import com.security.notes.repositories.UserRepository;
+import com.security.notes.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+
+    // Spring will auto-wire this (only one constructor → @Autowired is optional)
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository,
+                           RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public void updateUserRole(Long userId, String roleName) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+
+        AppRole appRole = AppRole.valueOf(roleName);
+
+        Role role = roleRepository.findByRoleName(appRole).orElseThrow(() -> new RuntimeException("Role not found"));
+
+        user.setRole(role);
+
+        userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public UserDTO getUserById(Long id) {
+//        return userRepository.findById(id).orElseThrow();
+        User user = userRepository.findById(id).orElseThrow();
+        return convertToDto(user);
+    }
+
+    private UserDTO convertToDto(User user) {
+        return new UserDTO(
+                user.getUserId(),
+                user.getUserName(),
+                user.getEmail(),
+                user.isAccountNonLocked(),
+                user.isAccountNonExpired(),
+                user.isCredentialsNonExpired(),
+                user.isEnabled(),
+                user.getCredentialsExpiryDate(),
+                user.getAccountExpiryDate(),
+                user.getTwoFactorSecret(),
+                user.isTwoFactorEnabled(),
+                user.getSignUpMethod(),
+                user.getRole(),
+                user.getCreatedDate(),
+                user.getUpdatedDate()
+        );
+    }
+
+}
